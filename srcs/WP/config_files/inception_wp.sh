da9d6ebe4a7e9b22049fa4d50a1d0ea3fae3d764
VictorDé <vducoulo@student.42lyon.fr>
2022-12-23 01:25:59 +0100

    update

--- srcs/WP/config_files/inception_wp.sh
+++ srcs/WP/config_files/inception_wp.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-if (cat /Users/victorducoulombier/Documents/dev/42/Inception/srcs/WP/config_files/test)
+if [ ! -f /var/www/html/index.php]
 then
-	echo "bonjour"
+	while [ ! -f /var/www/wordpress/wp-config.php]
+	do
+		wp config create --allow-root --dbhost=$WORDPRESS_DB_HOST --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --path=/var/www/html
+		echo "maybe it's installed ..."
+		sleep 2
+	done
+	wp core install --url=vducoulo.42.fr --title=Example --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com --allow-root --path=/var/www/html
+	echo "everything fine !!!!"
+	sleep infinity
 fi
-	echo "bye !"
+
+	echo "already installed"
+	sleep infinity
