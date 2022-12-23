7f028a7f55b94d90e56b04e84d1f694632fda1e9
VictorDé <vducoulo@student.42lyon.fr>
2022-12-23 19:08:38 +0100

    improved overall security

--- srcs/WP/config_files/inception_wp.sh
+++ srcs/WP/config_files/inception_wp.sh
@@ -3,12 +3,13 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	echo "looping"
 	while [ ! -f /var/www/html/wp-config.php ]
 	do
-		wp config create --allow-root --dbhost=$WORDPRESS_DB_HOST --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --path=/var/www/html
-		echo "maybe it's installed ..."
+		wp config create --allow-root --dbhost=$WORDPRESS_DB_HOST --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --path=/var/www/html
+		echo "configuration ok, starting installation..."
 		sleep 2
 	done
-	wp core install --url=vducoulo.42.fr --title=Example --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com --allow-root --path=/var/www/html
-	echo "everything fine !!!!"
+	wp core install --url=vducoulo.42.fr --title=vducouloLAND --admin_user=$WORDPRESS_ADMIN_NAME --admin_password=$WORDPRESS_ADMIN_PASS --admin_email=info@example.com --allow-root --path=/var/www/html
+	unset MYSQL_DATABASE MYSQL_USER MYSQL_PASSWORD MYSQL_ROOT_PASSWORD
+	echo "everything fine !"
 else
 	echo "already installed"
 fi
