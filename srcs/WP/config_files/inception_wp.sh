8084ff5fc1e74e8beee43f0476909089bc6278c8
VictorDé <vducoulo@student.42lyon.fr>
2022-12-23 09:22:13 +0100

    feat install script ok, need to change nginx config

--- srcs/WP/config_files/inception_wp.sh
+++ srcs/WP/config_files/inception_wp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ ! -f /var/www/html/index.php]
-then
-	while [ ! -f /var/www/wordpress/wp-config.php]
+if [ ! -f /var/www/html/wp-config.php ]; then
+	echo "looping motherfuckers"
+	while [ ! -f /var/www/html/wp-config.php ]
 	do
 		wp config create --allow-root --dbhost=$WORDPRESS_DB_HOST --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --path=/var/www/html
 		echo "maybe it's installed ..."
@@ -10,7 +10,9 @@ then
 	wp core install --url=vducoulo.42.fr --title=Example --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com --allow-root --path=/var/www/html
 	echo "everything fine !!!!"
 	sleep infinity
-fi
-
+else
 	echo "already installed"
+	sleep infinity
+fi
+	echo "EOS"
 	sleep infinity
