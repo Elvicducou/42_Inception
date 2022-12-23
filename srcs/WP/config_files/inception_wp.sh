0bd67e12297639dd43618ff8b9c8548bbe404aec
VictorDé <vducoulo@student.42lyon.fr>
2022-12-23 10:48:21 +0100

    feat web access ok + php configured on 9000

--- srcs/WP/config_files/inception_wp.sh
+++ srcs/WP/config_files/inception_wp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -f /var/www/html/wp-config.php ]; then
-	echo "looping motherfuckers"
+	echo "looping"
 	while [ ! -f /var/www/html/wp-config.php ]
 	do
 		wp config create --allow-root --dbhost=$WORDPRESS_DB_HOST --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --path=/var/www/html
@@ -9,10 +9,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	done
 	wp core install --url=vducoulo.42.fr --title=Example --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com --allow-root --path=/var/www/html
 	echo "everything fine !!!!"
-	sleep infinity
 else
 	echo "already installed"
-	sleep infinity
 fi
-	echo "EOS"
-	sleep infinity
+	php-fpm7.3 --nodaemonize
