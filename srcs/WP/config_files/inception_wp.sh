5a794f4af437e75b9bb97dd0a6f5c137a62a8261
VictorDé <vducoulo@student.42lyon.fr>
2022-12-25 17:54:01 +0100

    mandatory part ok, need to apply specific for vms changes

--- srcs/WP/config_files/inception_wp.sh
+++ srcs/WP/config_files/inception_wp.sh
@@ -8,7 +8,8 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 		sleep 2
 	done
 	wp core install --url=vducoulo.42.fr --title=vducouloLAND --admin_user=$WORDPRESS_ADMIN_NAME --admin_password=$WORDPRESS_ADMIN_PASS --admin_email=info@example.com --allow-root --path=/var/www/html
-	unset MYSQL_DATABASE MYSQL_USER MYSQL_PASSWORD MYSQL_ROOT_PASSWORD
+	wp user create $WORDPRESS_FIRSTUSER_NAME bob@example.com --allow-root --user_pass=$WORDPRESS_FIRSTUSER_PASS --role=author --path=/var/www/html
+	unset MYSQL_DATABASE MYSQL_USER MYSQL_PASSWORD MYSQL_ROOT_PASSWORD WORDPRESS_ADMIN_PASS WORDPRESS_ADMIN_NAME WORDPRESS_FIRSTUSER_NAME WORDPRESS_FIRSTUSER_PASS
 	echo "everything fine !"
 else
 	echo "already installed"
